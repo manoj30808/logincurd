dddbb1fe1f9635b04da1569c81d68a521d930916
manoj30808 <manoj30808@gmail.com>
2017-06-08 16:37:39 +0530

    first commit

--- src/resources/admin/setting/partials/form.blade.php
+++ src/resources/admin/setting/partials/form.blade.php
@@ -42,14 +42,14 @@
             </div>
         </div>
         <div class="form-group {{ $errors->has('twitter_client_id') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">twitter client id</label>
+            <label class="control-label col-lg-3" for="name">Twitter Client Id</label>
             <div class="col-lg-6">
                 {!! Form::text('twitter_client_id',old('twitter_client_id'),array('class'=>'form-control')) !!}
                 {!! $errors->first('twitter_client_id', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('twitter_client_secret') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">twitter client secret</label>
+            <label class="control-label col-lg-3" for="name">Twitter Client Secret</label>
             <div class="col-lg-6">
                 {!! Form::text('twitter_client_secret',old('twitter_client_secret'),array('class'=>'form-control')) !!}
                 {!! $errors->first('twitter_client_secret', '<span class="help-block">:message</span>') !!}
@@ -57,14 +57,14 @@
         </div>
 
         <div class="form-group {{ $errors->has('google_client_id') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">google client id</label>
+            <label class="control-label col-lg-3" for="name">Google Client Id</label>
             <div class="col-lg-6">
                 {!! Form::text('google_client_id',old('google_client_id'),array('class'=>'form-control')) !!}
                 {!! $errors->first('google_client_id', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('google_client_secret') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">google client secret</label>
+            <label class="control-label col-lg-3" for="name">Google Client Secret</label>
             <div class="col-lg-6">
                 {!! Form::text('google_client_secret',old('google_client_secret'),array('class'=>'form-control')) !!}
                 {!! $errors->first('google_client_secret', '<span class="help-block">:message</span>') !!}
@@ -72,14 +72,14 @@
         </div>
 
         <div class="form-group {{ $errors->has('facebook_client_id') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">facebook client id</label>
+            <label class="control-label col-lg-3" for="name">Facebook Client Id</label>
             <div class="col-lg-6">
                 {!! Form::text('facebook_client_id',old('facebook_client_id'),array('class'=>'form-control')) !!}
                 {!! $errors->first('facebook_client_id', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('facebook_client_secret') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">facebook client secret</label>
+            <label class="control-label col-lg-3" for="name">Facebook Client Secret</label>
             <div class="col-lg-6">
                 {!! Form::text('facebook_client_secret',old('facebook_client_secret'),array('class'=>'form-control')) !!}
                 {!! $errors->first('facebook_client_secret', '<span class="help-block">:message</span>') !!}
@@ -87,14 +87,14 @@
         </div>
 
         <div class="form-group {{ $errors->has('pinterest_client_id') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">pinterest client id</label>
+            <label class="control-label col-lg-3" for="name">Pinterest Client Id</label>
             <div class="col-lg-6">
                 {!! Form::text('pinterest_client_id',old('pinterest_client_id'),array('class'=>'form-control')) !!}
                 {!! $errors->first('pinterest_client_id', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('pinterest_client_secret') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">pinterest client secret</label>
+            <label class="control-label col-lg-3" for="name">Pinterest Client Secret</label>
             <div class="col-lg-6">
                 {!! Form::text('pinterest_client_secret',old('pinterest_client_secret'),array('class'=>'form-control')) !!}
                 {!! $errors->first('pinterest_client_secret', '<span class="help-block">:message</span>') !!}
@@ -102,14 +102,14 @@
         </div>
 
         <div class="form-group {{ $errors->has('linkedin_client_id') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">linkedin client id</label>
+            <label class="control-label col-lg-3" for="name">Linkedin Client Id</label>
             <div class="col-lg-6">
                 {!! Form::text('linkedin_client_id',old('linkedin_client_id'),array('class'=>'form-control')) !!}
                 {!! $errors->first('linkedin_client_id', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('linkedin_client_secret') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">linkedin client secret</label>
+            <label class="control-label col-lg-3" for="name">Linkedin Client Secret</label>
             <div class="col-lg-6">
                 {!! Form::text('linkedin_client_secret',old('linkedin_client_secret'),array('class'=>'form-control')) !!}
                 {!! $errors->first('linkedin_client_secret', '<span class="help-block">:message</span>') !!}
@@ -118,42 +118,42 @@
 
 
         <div class="form-group {{ $errors->has('mail_driver') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail driver</label>
+            <label class="control-label col-lg-3" for="name">Mail Driver</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_driver',old('mail_driver'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_driver', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('mail_host') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail host</label>
+            <label class="control-label col-lg-3" for="name">Mail Host</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_host',old('mail_host'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_host', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('mail_port') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail port</label>
+            <label class="control-label col-lg-3" for="name">Mail Port</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_port',old('mail_port'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_port', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('mail_username') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail username</label>
+            <label class="control-label col-lg-3" for="name">Mail Username</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_username',old('mail_username'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_username', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('mail_password') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail password</label>
+            <label class="control-label col-lg-3" for="name">Mail Password</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_password',old('mail_password'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_password', '<span class="help-block">:message</span>') !!}
             </div>
         </div>
         <div class="form-group {{ $errors->has('mail_encryption') ? 'has-error' : '' }}">
-            <label class="control-label col-lg-3" for="name">mail encryption</label>
+            <label class="control-label col-lg-3" for="name">Mail Encryption</label>
             <div class="col-lg-6">
                 {!! Form::text('mail_encryption',old('mail_encryption'),array('class'=>'form-control')) !!}
                 {!! $errors->first('mail_encryption', '<span class="help-block">:message</span>') !!}
