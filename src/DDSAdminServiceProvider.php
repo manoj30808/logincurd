5a5d5dd9679d6f506a5d246c52a2b70223b8bbe5
jayeshmsp <jayesh@msp-group.co.uk>
2017-06-22 15:28:57 +0530

    remove public

--- src/DDSAdminServiceProvider.php
+++ src/DDSAdminServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace MspPack\DDSAdmin;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class DDSAdminServiceProvider extends ServiceProvider {
 
@@ -24,29 +25,31 @@ class DDSAdminServiceProvider extends ServiceProvider {
             __DIR__.'/public' => public_path(),
         ]);
 
-        $settings = \DB::table('settings')->get()->first();
-        if (!empty($settings)) {
-        	\Config::set(['services.twitter.client_id'=>$settings->twitter_client_id]);		
-        	\Config::set(['services.twitter.client_secret'=>$settings->twitter_client_secret]);		
-        	
-        	\Config::set(['services.google.client_id'=>$settings->google_client_id]);		
-        	\Config::set(['services.google.client_secret'=>$settings->google_client_secret]);		
+        if (Schema::hasTable('settings')) {
+            $settings = \DB::table('settings')->get()->first();
+            if (!empty($settings)) {
+            	\Config::set(['services.twitter.client_id'=>$settings->twitter_client_id]);		
+            	\Config::set(['services.twitter.client_secret'=>$settings->twitter_client_secret]);		
+            	
+            	\Config::set(['services.google.client_id'=>$settings->google_client_id]);		
+            	\Config::set(['services.google.client_secret'=>$settings->google_client_secret]);		
 
-        	\Config::set(['services.facebook.client_id'=>$settings->facebook_client_id]);		
-        	\Config::set(['services.facebook.client_secret'=>$settings->facebook_client_secret]);		
+            	\Config::set(['services.facebook.client_id'=>$settings->facebook_client_id]);		
+            	\Config::set(['services.facebook.client_secret'=>$settings->facebook_client_secret]);		
 
-        	\Config::set(['services.pinterest.client_id'=>$settings->pinterest_client_id]);		
-        	\Config::set(['services.pinterest.client_secret'=>$settings->pinterest_client_secret]);
+            	\Config::set(['services.pinterest.client_id'=>$settings->pinterest_client_id]);		
+            	\Config::set(['services.pinterest.client_secret'=>$settings->pinterest_client_secret]);
 
-        	\Config::set(['services.linkedin.client_id'=>$settings->linkedin_client_id]);		
-        	\Config::set(['services.linkedin.client_secret'=>$settings->linkedin_client_secret]);		
-        	
-        	\Config::set(['mail.driver'=>$settings->mail_driver]);
-        	\Config::set(['mail.host'=>$settings->mail_host]);
-        	\Config::set(['mail.port'=>$settings->mail_port]);
-        	\Config::set(['mail.username'=>$settings->mail_username]);
-        	\Config::set(['mail.password'=>$settings->mail_password]);
-        	\Config::set(['mail.encryption'=>$settings->mail_encryption]);
+            	\Config::set(['services.linkedin.client_id'=>$settings->linkedin_client_id]);		
+            	\Config::set(['services.linkedin.client_secret'=>$settings->linkedin_client_secret]);		
+            	
+            	\Config::set(['mail.driver'=>$settings->mail_driver]);
+            	\Config::set(['mail.host'=>$settings->mail_host]);
+            	\Config::set(['mail.port'=>$settings->mail_port]);
+            	\Config::set(['mail.username'=>$settings->mail_username]);
+            	\Config::set(['mail.password'=>$settings->mail_password]);
+            	\Config::set(['mail.encryption'=>$settings->mail_encryption]);
+            }
         }
         \Config::set(['auth.providers.users.model'=>\MspPack\DDSAdmin\User::class]);
         \Config::set(['cache.default' => 'array' ]);
